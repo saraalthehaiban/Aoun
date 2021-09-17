7a53646b0570dfa0ad624dd92956fa2eb6d299e0
saraalthehaiban <90033854+saraalthehaiban@users.noreply.github.com>
2021-09-18 02:00:04 +0300

    Request community feature added

--- AOUN/AOUN/ViewController.swift
+++ AOUN/AOUN/ViewController.swift
@@ -6,14 +6,23 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
 
+    @IBOutlet var Name: UITextField!
+    @IBOutlet var Info: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func submit(_ sender: Any) {
+        let db = Firestore.firestore()
+                let n = Name.text
+                let d = Info.text
+                db.collection("Request").document().setData(["Title": n, "Description":d, "Status":"Pending"])
+    }
+    
 }
 
