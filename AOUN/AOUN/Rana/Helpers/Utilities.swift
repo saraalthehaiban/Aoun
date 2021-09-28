85035dbd56cfb261806a1fe99f9d33235b282a20
ranaalhajri <63373059+ranaalhajri@users.noreply.github.com>
2021-09-29 01:55:23 +0300

    update on validation

--- AOUN/AOUN/Rana/Helpers/Utilities.swift
+++ AOUN/AOUN/Rana/Helpers/Utilities.swift
@@ -1,10 +1,5 @@
 //
-//  Utilities.swift
-//  customauth
-//
-//  Created by Christopher Ching on 2019-05-09.
-//  Copyright © 2019 Christopher Ching. All rights reserved.
-//
+
 
 import Foundation
 import UIKit
--- AOUN/AOUN/Rana/Helpers/ViewControllers/SignupViewController.swift
+++ AOUN/AOUN/Rana/Helpers/ViewControllers/SignupViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import FirebaseAuth
+import Firebase
 
 class SignupViewController: UIViewController {
 
@@ -37,24 +39,104 @@ class SignupViewController: UIViewController {
     {
         errorLabel.alpha=0
         
-        Utilities.styleTextField(firstNameTextField)
-        Utilities.styleTextField(lastNameTextField)
-        Utilities.styleTextField(emailTextField)
-        Utilities.styleTextField(passwordTextField)
        
         
     }
+    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func validateFields() -> String?
+    {
+        //check all fields are filled
+        if
+            firstNameTextField.text?.trimmingCharacters(in:.whitespacesAndNewlines) == ""
+                ||
+                
+                lastNameTextField.text?.trimmingCharacters(in:.whitespacesAndNewlines)==""
+                ||
+                emailTextField.text?.trimmingCharacters(in:.whitespacesAndNewlines)==""
+                ||  passwordTextField.text?.trimmingCharacters(in:.whitespacesAndNewlines)==""
+        {
+            
+            return "please fill in all fields"
+        }
+        
+        //check if the password is secure
+        
+        let cleanedPassword = passwordTextField.text!.trimmingCharacters(in:.whitespacesAndNewlines)
+        if Utilities
+            .isPasswordValid(cleanedPassword) == false
+        {
+            return "Please make sure your is at least 8 characters, contains special character and a number."
+        }
+        
+        //email validation
+        
+        return nil
     }
-    */
-
     @IBAction func signUptapped(_ sender: Any) {
+       let error = validateFields()
+        if error != nil
+        {
+            
+            showError(error!)
+            
+        }
+        else
+        {
+            //create cleaned ref
+            
+            let firstName = firstNameTextField.text!.trimmingCharacters(in:.whitespacesAndNewlines)
+            let lastName =  lastNameTextField.text!.trimmingCharacters(in:.whitespacesAndNewlines)
+            let email = emailTextField.text!.trimmingCharacters(in:.whitespacesAndNewlines)
+            let password = passwordTextField.text!.trimmingCharacters(in:.whitespacesAndNewlines)
+            
+            Auth.auth().createUser(withEmail: email, password: password) {( result, err) in
+                
+                if  err != nil
+                
+                {
+                    
+                //there was an error
+                    self.showError("Error creating user")
+                }
+                else
+                {
+                    let db = Firestore.firestore()
+                    db.collection("users").addDocument(data: ["FirstName": firstName, "LastName": lastName, "uid": result!.user.uid]){(error)
+                        in
+                        if error != nil{
+                            
+                            self.showError("Try again please, error saving user's data")
+                        }
+                        
+                        
+                        
+                    }
+                    
+                    //created
+                    // to home screen
+                    
+                    
+                    self.transitionToHome()
+                    
+                }
+                
+            }
+        }
+        
+        
     }
+    
+    func showError(_ message:String) {
+        errorLabel.text = message
+        errorLabel.alpha = 1
+        
+    }
+        
+  func transitionToHome()
+    
+    {
+        
+    }
+    
 }
