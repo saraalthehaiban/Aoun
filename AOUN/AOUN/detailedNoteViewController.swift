5a583ed75843a9956d06f8beb11572cbc067847d
Rasha AlSughier <rasha.alsughier@gmail.com>
2021-10-29 18:37:53 +0300

    aded payment flow and download checks and alert

--- AOUN/AOUN/detailedNoteViewController.swift
+++ AOUN/AOUN/detailedNoteViewController.swift
@@ -6,8 +6,9 @@
 //
 
 import UIKit
-import BraintreeDropIn
 
+import Braintree
+import BraintreeDropIn
 import PayPalCheckout
 
 
@@ -20,7 +21,8 @@ enum DownloadAction : String {
 
 class detailedNoteViewController: UIViewController{
     let authorization = "sandbox_f252zhq7_hh4cpc39zq4rgjcg"
-
+    var braintreeClient: BTAPIClient?
+    
     @IBOutlet weak var topPic: UIImageView!
     @IBOutlet weak var noteTitleLable: UILabel!
     @IBOutlet weak var noteTitle: UILabel!
@@ -37,30 +39,21 @@ class detailedNoteViewController: UIViewController{
     
     var priceOfNote:Decimal = 0
     var hackCheck = false
-    
-    
+     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-//        let paymentButton = PayPalButton()
-//        paymentButton.frame = CGRect(x: 100, y: 240, width: 80, height: 44)
-//        paymentButton.backgroundColor = .red
-//        //self.view.insertSubview(paymentButton, at: 100)
-//        self.view.addSubview(paymentButton)
-        
         noteTitle.text = note.noteLable
         aoutherName.text = note.autherName
         desc.text = note.desc
-        price.text = note.price
+        price.text = "SAR \(note.price ?? "")"
         
-        if let p = note.price, p.count > 0, let price = Decimal(string: p), price > 0 {
+        if note.priceDecimal != nil {
             downloadButton.setTitle("Pay & Download", for: .normal)
-            priceOfNote = price
+            priceOfNote = note.priceDecimal ?? 0
         }
         
         // Do any additional setup after loading the view.
-        
-//        self.configurePayPalCheckout()
+        //self.configurePayPalCheckout()
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -70,7 +63,7 @@ class detailedNoteViewController: UIViewController{
             self.download(url: note.url!)
         }
     }
-
+    
     //@IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     @IBAction func downloadButtonTouched(_ sender: Any) {
         guard let url = note.url else {
@@ -79,42 +72,87 @@ class detailedNoteViewController: UIViewController{
             return
         }
         
+        
+        
         if priceOfNote > 0 {
             //payment
+            self.triggerPurchase(url: url)
             
-            //showDropIn(clientTokenOrTokenizationKey:authorization, url:url )
-            triggerPayPalCheckout()
         }else{
             download(url: url)
         }
     }
     
+    func triggerPurchase(url:URL) {
+        let title = "Purchase: \(note.noteLable) | SAR\(note.priceDecimal ?? 0) (USD\(note.usdPrice ?? 0))"
+        let activityViewController = UIAlertController(title: title, message: "You will be re-directed to paypal to confirm payment", preferredStyle: .actionSheet)
+        let purchaseAction = UIAlertAction(title: "Purchase", style: .default) { action in
+            self.paymentAction(url:url)
+        }
+        activityViewController.addAction(purchaseAction)
+        
+        let cancleAction = UIAlertAction(title: "Cancle", style: .cancel, handler: nil)
+        activityViewController.addAction(cancleAction)
+        self.present(activityViewController, animated: true, completion: nil)
+    }
+    
+    func paymentAction(url:URL){
+        //        self.triggerPayPalCheckout()
+        
+        //            self.showDropIn(clientTokenOrTokenizationKey: authorization, url: url) //Metod 2
+        
+        self.startCheckout(amount: "\(priceOfNote)") { message in
+            self.download(url: url)
+        } failure: { error in
+            //TODO: Show alert from sarah's code
+        }
+    }
+    
+    
     func download (url:URL) {
         //activityIndicator.startAnimating()
         DownloadManager.download(url: url) { success, data in
-            //guard let documentData = data.dataRe
-            //self.activityIndicator.stopAnimating()
-            let vcActivity = UIActivityViewController(activityItems: [data], applicationActivities: nil)
-            self.present(vcActivity, animated: true, completion: nil)
+            guard let d = data else{ return }
+            self.showFileSaveActivity(data: d)
         }
+    }
+    
+    func showFileSaveActivity (data:Data) {
+        let vcActivity = UIActivityViewController(activityItems: [data], applicationActivities: nil)
+        vcActivity.completionWithItemsHandler = {(activityType: UIActivity.ActivityType?, completed: Bool, returnedItems: [Any]?, error: Error?) in
+            if !completed {
+                // User canceled
+                return
+            }
+            // User completed activity
+            self.showDownloadSuccess()
+        }
+        self.present(vcActivity, animated: true, completion: nil)
+    }
+    
+    func showDownloadSuccess () {
+        let alertVC = UIAlertController(title: "Downloaded!", message: "File \"\(self.note.noteLable)\" dowloaded successfully.", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Ok", style: .cancel) { action in
+            self.dismiss(animated: true, completion: nil)
+        }
+        alertVC.addAction(action)
+        self.present(alertVC, animated: true, completion: nil)
     }
 }
 
 
 //MARK:- payment implementation
 extension detailedNoteViewController : BTThreeDSecureRequestDelegate {
-    
     func showDropIn(clientTokenOrTokenizationKey: String, url:URL) {
-        
         let request = BTDropInRequest()
-
+        
         let threeDSecureRequest = BTThreeDSecureRequest()
         threeDSecureRequest.threeDSecureRequestDelegate = self
-
+        
         threeDSecureRequest.amount = 1.00
         threeDSecureRequest.email = "test@example.com"
         threeDSecureRequest.versionRequested = .version2
-
+        
         let address = BTThreeDSecurePostalAddress()
         address.givenName = "Jill"
         address.surname = "Doe"
@@ -126,14 +164,14 @@ extension detailedNoteViewController : BTThreeDSecureRequestDelegate {
         address.postalCode = "12345"
         address.countryCodeAlpha2 = "US"
         threeDSecureRequest.billingAddress = address
-
+        
         
         // Optional additional information.
         // For best results, provide as many of these elements as possible.
         let additionalInformation = BTThreeDSecureAdditionalInformation()
         additionalInformation.shippingAddress = address
         threeDSecureRequest.additionalInformation = additionalInformation
-
+        
         request.threeDSecureRequest = threeDSecureRequest
         
         let dropIn = BTDropInController(authorization: clientTokenOrTokenizationKey, request: request)
@@ -160,44 +198,26 @@ extension detailedNoteViewController : BTThreeDSecureRequestDelegate {
         
     }
     
-//    func configurePayPalCheckout() {
-//        Checkout.setCreateOrderCallback { createOrderAction in
-//            let amount = PurchaseUnit.Amount(currencyCode: .usd, value: "\(self.priceOfNote)")
-//
-//            let purchaseUnit = PurchaseUnit(amount: amount, softDescriptor: self.note.noteLable)
-//            let order = OrderRequest(intent: .capture, purchaseUnits: [purchaseUnit])
-//
-//            createOrderAction.create(order: order)
-//        }
-//
-//        Checkout.setOnApproveCallback { approval in
-//            approval.actions.capture { (response, error) in
-//                print("Order successfully captured: \(response?.data)")
-//            }
-//        }
-//        Checkout.setOnCancelCallback {
-//            // User has cancelled the payment experience
-//        }
-//        Checkout.setOnErrorCallback { error in
-//            // Handle the error generated by the SDK
-//        }
-//
-//    }
-    
+    func returnToDownload() {
+        self.download(url: note.url!)
+    }
     
     func triggerPayPalCheckout() {
         self.hackCheck = true
-//        Checkout.addShippingAddress(givenName: "", familyName: "", address: "" as! Address)
+        //        Checkout.addShippingAddress(givenName: "", familyName: "", address: "" as! Address)
         Checkout.start(presentingViewController: self) { createOrder in
             let amount = PurchaseUnit.Amount(currencyCode: .usd, value: "\(self.priceOfNote)")
             let purchaseUnit = PurchaseUnit(amount: amount)
             let order = OrderRequest(intent: .capture, purchaseUnits: [purchaseUnit])
-
+            
             createOrder.create(order: order)
         } onApprove: { approval in
             approval.actions.capture { (response, error) in
+                self.returnToDownload()
                 print("Order successfully captured: \(response?.data)")
+                //self.returnToDownload() //DID NOT WORK
             }
+            //self.returnToDownload() //DID NOT WORK
         } onShippingChange: { shippingChange, shippingChangeAction in
             //
         } onCancel: {
@@ -206,37 +226,75 @@ extension detailedNoteViewController : BTThreeDSecureRequestDelegate {
             print("Order onError captur")
         }
         
-        self.download(url: note.url!)
-
+        //self.download(url: note.url!)
         
-//        Checkout.start(
-//            createOrder: { createOrderAction in
-//
-//                let amount = PurchaseUnit.Amount(currencyCode: .usd, value: "\(self.priceOfNote)")
-//                let purchaseUnit = PurchaseUnit(amount: amount)
-//                let order = OrderRequest(intent: .capture, purchaseUnits: [purchaseUnit])
-//
-//                createOrderAction.create(order: order)
-//
-//            }, onApprove: { approval in
-//
-//                approval.actions.capture { (response, error) in
-//                    print("Order successfully captured: \(response?.data)")
-//                }
-//
-//            }, onCancel: {
-//
-//                print("Order onCancel captur")
-//                // Optionally use this closure to respond to the user canceling the paysheet
-//
-//            }, onError: { error in
-//
-//                print("Order onError captur")
-//                // Optionally use this closure to respond to the user experiencing an error in
-//                // the payment experience
-//
-//            }
-//        )
+        //        Checkout.start(
+        //            createOrder: { createOrderAction in
+        //
+        //                let amount = PurchaseUnit.Amount(currencyCode: .usd, value: "\(self.priceOfNote)")
+        //                let purchaseUnit = PurchaseUnit(amount: amount)
+        //                let order = OrderRequest(intent: .capture, purchaseUnits: [purchaseUnit])
+        //
+        //                createOrderAction.create(order: order)
+        //
+        //            }, onApprove: { approval in
+        //
+        //                approval.actions.capture { (response, error) in
+        //                    print("Order successfully captured: \(response?.data)")
+        //                }
+        //
+        //            }, onCancel: {
+        //
+        //                print("Order onCancel captur")
+        //                // Optionally use this closure to respond to the user canceling the paysheet
+        //
+        //            }, onError: { error in
+        //
+        //                print("Order onError captur")
+        //                // Optionally use this closure to respond to the user experiencing an error in
+        //                // the payment experience
+        //
+        //            }
+        //        )
+    }
+    
+    func startCheckout(amount:String, success: @escaping (String)->Void, failure: @escaping (Error) -> Void) {
+        braintreeClient = BTAPIClient(authorization: authorization)
+        if let btClient = braintreeClient {
+            let payPalDriver = BTPayPalDriver(apiClient: btClient)
+            let request = BTPayPalCheckoutRequest(amount: amount)
+            request.currencyCode = "USD"
+            
+            /*let amount = PurchaseUnit.Amount(currencyCode: .usd, value: "\(self.priceOfNote)")
+             let purchaseUnit = PurchaseUnit(amount: amount)
+             let order = OrderRequest(intent: .capture, purchaseUnits: [purchaseUnit])
+             createOrder.create(order: order)*/
+            
+            payPalDriver.tokenizePayPalAccount(with: request) { (tokenizedPayPalAccount, error) in
+                if let tokenizedPayPalAccount = tokenizedPayPalAccount {
+                    print("Got a nonce: \(tokenizedPayPalAccount.nonce)")
+                    
+                    
+                    let email = tokenizedPayPalAccount.email
+                    let firstName = tokenizedPayPalAccount.firstName
+                    let lastName = tokenizedPayPalAccount.lastName
+                    let phone = tokenizedPayPalAccount.phone
+                    
+                    let billingAddress = tokenizedPayPalAccount.billingAddress
+                    let shippingAddress = tokenizedPayPalAccount.shippingAddress
+                    
+                    success(email ?? "")
+                } else if let error = error {
+                    
+                    print(error.localizedDescription)
+                    failure(error)
+                    self.errorMsg.text = error.localizedDescription
+                } else {
+                    
+                    
+                }
+            }
+        }
     }
 }
 
