43029e70abc7db9f2f53667909cf3f1a93f38d0b
ReemaHaq <90069679+ReemaHaq@users.noreply.github.com>
2021-11-02 19:05:51 +0300

    updating post workshop

--- AOUN/AOUN/Reema/postWorkshopViewController.swift
+++ AOUN/AOUN/Reema/postWorkshopViewController.swift
@@ -11,17 +11,17 @@ import Firebase
 import UniformTypeIdentifiers
 
 protocol postWorkshopViewControllerDelegate{
-    func postWorkshop(_ vc: postWorkshopViewController,/* resource: resFile?,*/ added: Bool)
+    func postWorkshop(_ vc: postWorkshopViewController, workshop: workshopFile?, added: Bool)
 }
 
-class postWorkshopViewController: UIViewController {
+class postWorkshopViewController: UIViewController, UIDocumentPickerDelegate, UITextViewDelegate, UITextFieldDelegate {
     
     var delegate: postWorkshopViewControllerDelegate?
     
     @IBOutlet weak var background: UIImageView!
     @IBOutlet weak var stack: UIStackView!
     
-    @IBOutlet weak var title: UITextField!
+    @IBOutlet weak var Wtitle: UITextField!
     @IBOutlet weak var presenter: UITextField!
     @IBOutlet weak var priceTextbox: UITextField!
     @IBOutlet weak var seat: UITextField!
@@ -34,9 +34,9 @@ class postWorkshopViewController: UIViewController {
     
     @IBAction func submit(_ sender: UIButton) {
         
-        if title.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||  presenter.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" || priceTextbox.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" || seat.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" || descV.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "*Description" /* || date*/ {
-                     if title.text?.trimmingCharacters(in: .whitespacesAndNewlines) == ""{
-                        title.attributedPlaceholder = NSAttributedString(string: "*Title",
+        if Wtitle.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||  presenter.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" || priceTextbox.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" || seat.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" || descV.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "*Description" /* || date*/ {
+                     if Wtitle.text?.trimmingCharacters(in: .whitespacesAndNewlines) == ""{
+                        Wtitle.attributedPlaceholder = NSAttributedString(string: "*Title",
 
                                                       attributes: [NSAttributedString.Key.foregroundColor: UIColor.red])
                      }
@@ -55,14 +55,14 @@ class postWorkshopViewController: UIViewController {
 
                                             attributes: [NSAttributedString.Key.foregroundColor: UIColor.red])
            }
-            if descV.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "*Description"{
-                descV.attributedPlaceholder = NSAttributedString(string: "*Description",
-
-                                            attributes: [NSAttributedString.Key.foregroundColor: UIColor.red])
-           }
+//            if descV.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "*Description"{
+//                descV.attributedPlaceholder = NSAttributedString(string: "*Description",
+//
+//                                            attributes: [NSAttributedString.Key.foregroundColor: UIColor.red])
+//           }
                  }
         
-         if title.text?.trimmingCharacters(in: .whitespacesAndNewlines) == ""{
+         if Wtitle.text?.trimmingCharacters(in: .whitespacesAndNewlines) == ""{
              msg.attributedText = NSAttributedString(string: "Please fill in the Title.",
 
                                                      attributes: [NSAttributedString.Key.foregroundColor: UIColor.red])
@@ -89,16 +89,16 @@ class postWorkshopViewController: UIViewController {
 
         }
         
-        if title.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" &&  presenter.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" && priceTextbox.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" && seat.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" && descV.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "*Description" /* && date*/ {
+        if Wtitle.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" &&  presenter.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" && priceTextbox.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" && seat.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" && descV.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "*Description" /* && date*/ {
             msg.attributedText = NSAttributedString(string: "Please fill in all missing fields.",
 
                                                     attributes: [NSAttributedString.Key.foregroundColor: UIColor.red])
         }
         
-        if title.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||  presenter.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" || priceTextbox.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" || seat.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" || descV.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "*Description" /* || date*/ {
+        if Wtitle.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||  presenter.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" || priceTextbox.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" || seat.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" || descV.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "*Description" /* || date*/ {
             return}
         
-        let editedTitle = title.text?.trimmingCharacters(in: .whitespacesAndNewlines)
+        let editedTitle = Wtitle.text?.trimmingCharacters(in: .whitespacesAndNewlines)
         let editedPresenter = presenter.text?.trimmingCharacters(in: .whitespacesAndNewlines)
         let editedDesc =  descV.text?.trimmingCharacters(in: .whitespacesAndNewlines)
 
@@ -125,59 +125,29 @@ class postWorkshopViewController: UIViewController {
         
         msg.text = ""
         
-        
-//        print("Selected file paths", files, localFile)
-//        let filename = localFile.lastPathComponent
-        
-//        let uid = Auth.auth().currentUser?.uid ?? ""
-//        print("uid = ", uid, filename)
-//
-//        let storageWorkshop = Storage.storage().reference()
-        //let resRef = storageRef.child("Resources/\(uid)/\(filename)")
-//        let workshopRef = storageRef.child("Workshop/\(uid)")
-//
-//        let ut = resRef.putData(documentFileData, metadata: nil) { metaData, error in
-//            if let e =  error {
-//                print (e)
-//                self.msg.attributedText = NSAttributedString(string: "File couldn't be uploaded.",
-//                                                             attributes: [NSAttributedString.Key.foregroundColor: UIColor.red])
-//                return
-//            }
-//            guard let metadata = metaData else {
-//                self.msg.attributedText = NSAttributedString(string: "File couldn't be uploaded.",
-//                                                             attributes: [NSAttributedString.Key.foregroundColor: UIColor.red])
-//                return
-//            }
-//            resRef.downloadURL { (url, error) in
-//                guard let downloadURL = url else {
-//                    //an error occurred!
-//                    return
-//                }
-//                self.createDocument(with : downloadURL)
-//            }
-//        }
         self.createDocument()
-        ut.resume()
     } //end func submit
     
     
     func createDocument() {
         let db = Firestore.firestore()
-        let title = resourceV.text!
-        let presenter = authorV.text!
-        let desc = descV.text
+        let ti = Wtitle.text!
+        let pres = presenter.text!
+        let price =  priceTextbox.text!
+        let se =  seat.text!
+        let desc = descV.text!
         
-        let data = ["ResName": res, "authorName": author, "pubName":pub, "desc":desc, "url":url, "uid":Auth.auth().currentUser?.uid]
-        let resource = resFile(name: res, author: author, publisher: pub, desc: desc, urlString: url)
-        
-        db.collection("Resources").document().setData(data) { error in
+        let data = ["title": ti, "presenter": pres, "price": price, "seat": se, "desc":desc,  "uid":Auth.auth().currentUser?.uid]
+        let workshop = workshopFile(Title: ti, Presenter: pres, Price: price, Seat: se, Description: desc)
+
+        db.collection("Workshop").document().setData(data) { error in
             if let e = error {
                 print (e)
-                self.delegate?.resPost(self, resource: resource, added: false)
+                self.delegate?.postWorkshop(self, workshop: workshop, added: false)
                 return
             } else {
                 //Show susccess message and go out
-                let alert = UIAlertController.init(title: "Posted", message: "Your resource posted successfully.", preferredStyle: .alert)
+                let alert = UIAlertController.init(title: "Posted", message: "Your workshop posted successfully.", preferredStyle: .alert)
                 alert.view.tintColor = .black
                 var imageView = UIImageView(frame: CGRect(x: 125, y: 60, width: 20, height: 20))
                 imageView.image = UIImage(named: "Check")
@@ -185,7 +155,7 @@ class postWorkshopViewController: UIViewController {
                 let cancleA = UIAlertAction(title: "Ok", style: .cancel) { action in
                     self.dismiss(animated: true) {
                         //inform main controller t update the information
-                        self.delegate?.resPost(self, resource: resource, added: true)
+                        self.delegate?.postWorkshop(self, workshop: workshop, added: true)
                     }
                 }
                 alert.addAction(cancleA)
@@ -196,8 +166,8 @@ class postWorkshopViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        resourceV.delegate = self
-        authorV.delegate = self
+        Wtitle.delegate = self
+        presenter.delegate = self
         descV.delegate = self
         descV.layer.borderColor = #colorLiteral(red: 0.7685510516, green: 0.7686814666, blue: 0.7771411538, alpha: 1)
         descV.text = "Description"
@@ -207,13 +177,13 @@ class postWorkshopViewController: UIViewController {
     } //end func viewDidLoad
     
     
-    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        let maxLength = 20
-        let currentString: NSString = (textField.text ?? "") as NSString
-        let newString: NSString =
-            currentString.replacingCharacters(in: range, with: string) as NSString
-        return newString.length <= maxLength
-    }
+//    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+//        let maxLength = 20
+//        let currentString: NSString = (textField.text ?? "") as NSString
+//        let newString: NSString =
+//            currentString.replacingCharacters(in: range, with: string) as NSString
+//        return newString.length <= maxLength
+//    }
    
     
     // how?
@@ -224,6 +194,13 @@ class postWorkshopViewController: UIViewController {
             let allowedCharacters = CharacterSet(charactersIn:"0123456789")
             let characterSet = CharacterSet(charactersIn: string)
             return allowedCharacters.isSuperset(of: characterSet)
+        }else{
+            let maxLength = 20
+            let currentString: NSString = (textField.text ?? "") as NSString
+            let newString: NSString =
+                currentString.replacingCharacters(in: range, with: string) as NSString
+            return newString.length <= maxLength
+            
         }
 //        else if textField == noteTitleTextbox {
 //            if let t = textField.text, t.count > 24 {return false}
@@ -242,9 +219,9 @@ class postWorkshopViewController: UIViewController {
     
     //[2] placeholder
     func textViewDidBeginEditing(_ textView: UITextView) {
-                if descriptionTextbox.textColor == #colorLiteral(red: 0.7685510516, green: 0.7686815858, blue: 0.7814407945, alpha: 1) || descriptionTextbox.textColor == UIColor.red{
-                    descriptionTextbox.text = nil
-                    descriptionTextbox.textColor = UIColor.black
+                if descV.textColor == #colorLiteral(red: 0.7685510516, green: 0.7686815858, blue: 0.7814407945, alpha: 1) || descV.textColor == UIColor.red{
+                    descV.text = nil
+                    descV.textColor = UIColor.black
                 }
             }
     
