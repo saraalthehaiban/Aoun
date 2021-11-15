80a8e3b50990f8cfbc50ea04e4f2f6e3da045f64
ReemaHaq <90069679+ReemaHaq@users.noreply.github.com>
2021-11-15 20:46:54 +0300

    storyboard resource note

--- AOUN/AOUN/Shatha/ChangePasswordVC.swift
+++ AOUN/AOUN/Shatha/ChangePasswordVC.swift
@@ -10,63 +10,105 @@ import FirebaseAuth
 import Firebase
 
 class ChangePasswordVC: UIViewController {
-
+    
+    @IBOutlet weak var currentPassword: UITextField!
     @IBOutlet weak var newPass: UITextField!
     @IBOutlet weak var rePass: UITextField!
     @IBOutlet weak var errorMSG: UILabel!
+   
     @IBAction func changePassword(_ sender: UIButton) {
-        errorMSG.text = validatPassword()  //fix the logic
-        let password = newPass.text
-        var email = ""
-        getEmail { [self] (uEmail) in
-            email = uEmail
+        if let errorMessage = validatPassword() {
+            errorMSG.text =  errorMessage  //fix the logic
+            return
         }
-        if (errorMSG.text == nil && password != nil){
+        
+        guard let email = Auth.auth().currentUser?.email else {
+            errorMSG.text = "Email not found!"
+            return
+        }
+        let currentPassword = currentPassword.text!
+        let password = newPass.text!
+        
+        Auth.auth().signIn(withEmail: email, password: currentPassword) { result, error in
+            guard let res = result, error == nil else {
+                print ("Error = ", error!)
+                self.errorMSG.text = error?.localizedDescription
+                return
+            }
+            
+            print("Result = ", res)
+            Auth.auth().currentUser?.updatePassword(to: password, completion: { error in
+                if let e = error {
+                    self.errorMSG.text = e.localizedDescription
+                    return
+                }
+                
+                CustomAlert.showAlert(title: "Password Updated", message: "Password changed successfully. You will be logged out now, please sign in again.", inController: self, cancleTitle: "Ok") {
+                    try? Auth.auth().signOut()
+                    let appD = UIApplication.shared.delegate as! AppDelegate
+                    appD.setRoot()
+                }
+            })
+            
+        }
+        
+        
+        
+       /* if (errorMSG.text == nil && password != nil){
             let user = Auth.auth().currentUser
             var credential: AuthCredential
             var credentials = EmailAuthProvider.credential(withEmail: email, password: password!)
             // Prompt the user to re-provide their sign-in credentials
-
+            
             user?.reauthenticate(with: credentials) { error,arg  in
-              if let error = error {
-                // An error happened.
-              } else {
-                Auth.auth().currentUser?.updatePassword(to: password!) { (error) in              }
+                if let error = error {
+                    // An error happened.
+                } else {
+                    Auth.auth().currentUser?.updatePassword(to: password!) { (error) in              }
+                    //Auth.auth().sign
+                }
             }
-        }
-        }}
-  
+        }*/
+        
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       
-    
+        
+        
     }
     
     func validatPassword() -> String? {
+        if currentPassword.text?.trimmingCharacters(in:.whitespacesAndNewlines)=="" {
+            //            passwordTextField.textColor = .red
+            currentPassword.attributedPlaceholder = NSAttributedString(string: "*Password",
+                                                               attributes: [NSAttributedString.Key.foregroundColor: UIColor.red])
+            return "please enter current password"
+        }
         
         if newPass.text?.trimmingCharacters(in:.whitespacesAndNewlines)=="" {
             //            passwordTextField.textColor = .red
             newPass.attributedPlaceholder = NSAttributedString(string: "*Password",
-                                                                         attributes: [NSAttributedString.Key.foregroundColor: UIColor.red])
+                                                               attributes: [NSAttributedString.Key.foregroundColor: UIColor.red])
             return "please fill missing fields"
         }
-       
+        
         
         
         if rePass.text?.trimmingCharacters(in:.whitespacesAndNewlines)==""
         {
             rePass.attributedPlaceholder = NSAttributedString(string: "*Confirm Password",
-                                                                         attributes: [NSAttributedString.Key.foregroundColor: UIColor.red])
+                                                              attributes: [NSAttributedString.Key.foregroundColor: UIColor.red])
             
             return "please fill in missing fields"
         }
-        
+        print("password Check: ", newPass.text, rePass.text)
         if(newPass.text != rePass.text)
         {
-        return "password doesn't match"
-             
-
+            return "password doesn't match"
+            
+            
         }
         let cleanedPassword = newPass.text!.trimmingCharacters(in:.whitespacesAndNewlines)
         if Utilities.isPasswordValid(cleanedPassword) == false
@@ -76,23 +118,22 @@ class ChangePasswordVC: UIViewController {
         return nil 
     }
     
-    func getEmail(completion: @escaping((String) -> ())) {
+    func getEmail(completion: @escaping((String?) -> ())) {
         let user = Auth.auth().currentUser
         if let user = user {
             let theEmail = user.email ?? ""
             completion(theEmail)
         }
-        
     }//end of getEmail
     
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
