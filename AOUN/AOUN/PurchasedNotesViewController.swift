2f4d9b131a2bf10bca15c0a26f578768b3e2684d
Rasha AlSughier <rasha.alsughier@gmail.com>
2021-11-24 00:25:23 +0300

    Purchased notes files + interface

---
+++ AOUN/AOUN/PurchasedNotesViewController.swift
@@ -0,0 +1,16 @@
+//
+//  PurchasedNotesViewController.swift
+//  AOUN
+//
+//  Created by Rasha on 24/11/2021.
+//
+
+import Foundation
+
+import UIKit
+import FirebaseStorage
+import Firebase
+
+class PurchasedNotesViewController: UIViewController, UISearchBarDelegate, UISearchDisplayDelegate {
+
+}
