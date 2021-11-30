c9f1ddceada0bdbf4c2eb2a04c0e490af7509df5
saraalthehaiban <90033854+saraalthehaiban@users.noreply.github.com>
2021-11-30 19:18:23 +0300

    Color and no chats label

--- AOUN/AOUN/Rana/CharRoster.swift
+++ AOUN/AOUN/Rana/CharRoster.swift
@@ -24,6 +24,7 @@ class UserCell : UITableViewCell {
 
 class VCChatRoster : UIViewController {
     @IBOutlet var tableView: UITableView!
+    @IBOutlet var noMessageLabel: UILabel!
     let db = Firestore.firestore()
     var chats : [Chat] = [] {
         didSet {
@@ -38,6 +39,7 @@ class VCChatRoster : UIViewController {
     }
     
     func loadChatRoster() {
+        noMessageLabel.text = ""
         guard let uid = Auth.auth().currentUser?.uid else {
             return
         }
@@ -45,7 +47,10 @@ class VCChatRoster : UIViewController {
             if let e = error {
                 print("Error: ", e.localizedDescription)
             } else {
-                guard let sds  = querySnapshot?.documents else {return}
+                guard let sds  = querySnapshot?.documents, sds.count > 0 else {
+                    self.noMessageLabel.text = "No chats"
+                    return
+                }
                 
                 self.chats.removeAll()
                 var cs : [Chat] = []
@@ -91,7 +96,7 @@ extension VCChatRoster:UITableViewDelegate, UITableViewDataSource {
             cell = c
         }else {
             cell = UserCell(style: UITableViewCell.CellStyle.value1, reuseIdentifier: "ci_chatUser")
-            cell.detailTextLabel?.textColor = .green
+            cell.detailTextLabel?.textColor = #colorLiteral(red: 0.2745098174, green: 0.4862745106, blue: 0.1411764771, alpha: 1)
             cell.detailTextLabel?.font = .boldSystemFont(ofSize: 18)
             cell.accessoryType = .disclosureIndicator
         }
